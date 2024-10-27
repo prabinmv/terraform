61f39d205a9937f9aab6a36df24dd9ad88b3abc9
prabinmv <prabinmv@live.in>
2024-10-27 14:47:11 +0530

    for each

--- terraform-variables/c5-ec2instance.tf
+++ terraform-variables/c5-ec2instance.tf
@@ -1,3 +1,10 @@
+data "aws_availability_zones" "myazone" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
@@ -5,6 +12,8 @@ resource "aws_instance" "web" {
   # instance_type = var.instance_type_map["dev"]
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
+  for_each = toset(data.aws_availability_zones.myazone.names)
+  availability_zone = each.key
   count = 2
   tags = {
     Name = "ec2-demo-${count.index}"
